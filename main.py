142b1e8949079e4bf17b5b10c96e306945fa0c6a
Piotr Czupyk <piotr.czupyk@gmail.com>
2020-04-14 20:16:09 +0200

    new

---
+++ main.py
@@ -0,0 +1,11 @@
+import mysql.connector as mariadb
+
+mariadb_connection = mariadb.connect(
+    user='root',
+    password='piotr',
+    database='db1')
+cursor = mariadb_connection.cursor()
+imie = ''
+cursor.execute("SELECT name FROM test LIMIT 1")
+
+print(cursor.execute)
