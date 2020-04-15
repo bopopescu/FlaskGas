9dd2cc02d00d145d53f13322027b97fcce4d2fd1
Piotr Czupyk <piotr.czupyk@gmail.com>
2020-04-15 23:50:26 +0200

    dbgood

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
+
 import mysql.connector as mariadb
+from flask import render_template
+from flask import request, redirect, url_for, flash
+import os
+from flask import app,config
+from flask import Flask
 
-mariadb_connection = mariadb.connect(
-    user='root',
-    password='piotr',
-    database='db1')
+app = Flask(__name__)
+
+app.config.update(dict(
+    SECRET_KEY='bardzosekretnawartosc',
+    SITE_NAME='GasCalculate'
+))
+
+def dblog():
+    global cursor
+    global mariadb_connection
+    mariadb_connection = mariadb.connect(user='root',
+        password='piotr',
+        database='db')
+dblog()
+
 cursor = mariadb_connection.cursor()
-imie = ''
-cursor.execute("SELECT name FROM test LIMIT 1")
+# cursor.execute('INSERT INTO jeden (value, date, time) VALUES (%s, %s, %s)', (value,date,time))
+# mariadb_connection.commit()
 
-print(cursor.execute)
-print('eny')
-def szs():
-    print('gg')
-print('gg') 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/calc')
+
+def Calculate():
+    return render_template('calc.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
