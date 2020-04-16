7d652fed98e85448d21b6af320b4b55504650dfb
Piotr Czupyk <piotr.czupyk@gmail.com>
2020-04-17 01:25:04 +0200

    prawie koniec

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-
 import mysql.connector as mariadb
 from flask import render_template
-from flask import request, redirect, url_for, flash
-import os
-from flask import app,config
+from flask import flash, redirect, url_for, request
+import time
+from datetime import date
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -16,23 +16,64 @@ app.config.update(dict(
 def dblog():
     global cursor
     global mariadb_connection
-    mariadb_connection = mariadb.connect(user='root',
+    mariadb_connection = mariadb.connect(
+        user='root',
         password='piotr',
         database='db')
-dblog()
 
-cursor = mariadb_connection.cursor()
+
+# cursor = mariadb_connection.cursor()
 # cursor.execute('INSERT INTO jeden (value, date, time) VALUES (%s, %s, %s)', (value,date,time))
 # mariadb_connection.commit()
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+
     return render_template('home.html')
+a= ''
 
-@app.route('/calc')
-
+@app.route('/calc',methods=['GET', 'POST'])
 def Calculate():
+    if request.method == 'POST':
+        global date
+        valuePage = request.form['zadanie'].strip()
+        if len(valuePage) > 0 :
+            dblog()
+            date=date.today()
+            timestamp=str(round(time.time()))
+
+            cursor = mariadb_connection.cursor()
+            cursor.execute("SELECT * FROM jeden ORDER BY id DESC LIMIT 1")
+            dbResult = cursor.fetchone()
+            dbValue = dbResult[1];dbDate = dbResult[2];dbTimestamp = dbResult[3]
+
+            valuePage=int(valuePage);dbValue=int(dbValue)
+            value = valuePage-dbValue
+            price = round(value*2.31)
+
+            timestamp=int(timestamp);dbTimestamp=int(dbTimestamp)
+            lapsing = round(timestamp-dbTimestamp)
+            lapsing = round(lapsing/86400)
+
+
+            cursor = mariadb_connection.cursor()
+            cursor.execute('INSERT INTO jeden (value, date, time) VALUES (%s, %s, %s)', (valuePage,date,timestamp))
+            mariadb_connection.commit()
+
+        price=str(price);lapsing=str(lapsing);dbDate=str(dbDate);value=str(value)
+        flash('Przez ostatnie '+lapsing+' dni (liczone od '+dbDate+'), zostalo zuzyte '+value+' kubikow gazu.(Co kosztowalo nas '
+          +price+' zl)')
     return render_template('calc.html')
+
+
+@app.route('/DataBase',methods=['GET', 'POST'])
+def dbList():
+    dblog()
+    cursor = mariadb_connection.cursor()
+    cursor.execute("SELECT * FROM jeden ORDER BY value DESC LIMIT 10")
+    dbResultlist = cursor.fetchone()
+    flash(dbResultlist)
+    return render_template('list.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
